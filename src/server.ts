e158b64a4b6a0b3d2221e1b89a62cae696d8c3ba
Cesar Oliveira <cesarsdoliveira@gmail.com>
2021-02-09 20:26:38 +1100

    Created database, migrations and dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: "Happy API"});
-})
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.json({ message: "Happy API" });
+});
 
 app.listen(3333);
